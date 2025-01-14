9ac04e2904da2ca3bf90579e654999d4f48bfd4a
ashpynov <ashpynov@gmail.com>
2025-01-14 15:07:06 +0300

    Redisign of GamepadState

--- Models/GamepadState.cs
+++ Models/GamepadState.cs
@@ -12,22 +12,18 @@ namespace ThemeOptions.Models
     {
         Gamepad _gamepad;
 
-        public new object this[string button]
+        private bool IsPressed(string button)
         {
-            get
+            if (_gamepad == null)
             {
-                if (_gamepad == null)
-                {
-                    _gamepad = new Gamepad();
-                    _gamepad.ButtonChanged += (o, e) => OnPropertyChanged(e);
-                }
-                return _gamepad.IsPressed(button);
+                _gamepad = new Gamepad();
+                _gamepad.ButtonChanged += (o, e) => OnPropertyChanged(e);
             }
-            set { }
+            return _gamepad.IsPressed(button);
         }
+
         public GamepadState()
         {
-
         }
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -35,5 +31,32 @@ namespace ThemeOptions.Models
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        public bool Start { get => IsPressed("Start"); }
+        public bool Back { get => IsPressed("Back"); }
+        public bool LeftStick { get => IsPressed("LeftStick"); }
+        public bool RightStick { get => IsPressed("RightStick"); }
+        public bool LeftShoulder { get => IsPressed("LeftShoulder"); }
+        public bool RightShoulder { get => IsPressed("RightShoulder"); }
+        public bool Guide { get => IsPressed("Guide"); }
+        public bool A { get => IsPressed("A"); }
+        public bool B { get => IsPressed("B"); }
+        public bool X { get => IsPressed("X"); }
+        public bool Y { get => IsPressed("Y"); }
+        public bool DPadLeft { get => IsPressed("DPadLeft"); }
+        public bool DPadRight { get => IsPressed("DPadRight"); }
+        public bool DPadUp { get => IsPressed("DPadUp"); }
+        public bool DPadDown { get => IsPressed("DPadDown"); }
+        public bool TriggerLeft { get => IsPressed("TriggerLeft"); }
+        public bool TriggerRight { get => IsPressed("TriggerRight"); }
+        public bool LeftStickLeft { get => IsPressed("LeftStickLeft"); }
+        public bool LeftStickRight { get => IsPressed("LeftStickRight"); }
+        public bool LeftStickUp { get => IsPressed("LeftStickUp"); }
+        public bool LeftStickDown { get => IsPressed("LeftStickDown"); }
+        public bool RightStickLeft { get => IsPressed("RightStickLeft"); }
+        public bool RightStickRight { get => IsPressed("RightStickRight"); }
+        public bool RightStickUp { get => IsPressed("RightStickUp"); }
+        public bool RightStickDown { get => IsPressed("RightStickDown"); }
+
     }
 }
--- Models/SettingsViewModel.cs
+++ Models/SettingsViewModel.cs
@@ -149,10 +149,6 @@ namespace ThemeOptions.Models
             {
                 Settings.OnPropertyChanged("Options");
             };
-            Settings.GamepadState.PropertyChanged += (o, e) =>
-            {
-                Settings.OnPropertyChanged(nameof(Settings.GamepadState));
-            };
         }
 
 
